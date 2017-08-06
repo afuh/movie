0bf85475ce7c575e07bbabcb8ea5114171ca837e
Axel <axelfuh@gmail.com>
2017-08-06 14:19:26 +0200

    Basic person page

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NavLink, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-const Person = () => {
-  return (
-    <div className="p">
-      person
-    </div>
+import { siteName, img } from '../helpers/utils';
+import { request } from '../helpers/api';
 
-  )
+class Person extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount(){
+    this.request()
+  }
+  request(){
+    request(`/person/${this.props.match.params.id}?`, 'credits,images')
+    .then(data => this.setState({data}))
+  }
+  render(){
+    const {name, biography, profile_path, credits} = this.state.data
+    console.log(this.state.data);
+    return(
+      <div className="">
+        <h1>{name && name}</h1>
+        <img src={profile_path && img(profile_path, "w300")} style={{width: "300px"}} className="" alt=""/>
+        <p>{biography && biography}</p>
+        <ul>
+          {credits && credits.cast.map((res, i) => {
+              return (
+                <li key={i}>
+                  <Link to={`/m/${res.id}`} className="title">{res.name ? res.name : res.title}</Link>
+                  <span> as {res.character}</span>
+                </li>
+              )
+          })}
+        </ul>
+      </div>
+
+    )
+  }
 }
 
 export default Person;
